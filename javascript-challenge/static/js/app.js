39cb461630f8827c2bbdfca4bf19587a585fa1f2
g4techcyber <52834551+g4techcyber@users.noreply.github.com>
2020-06-27 17:41:00 -0400

    for java script home work

---
+++ javascript-challenge/static/js/app.js
@@ -0,0 +1,73 @@
+// Variables
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datepicker1");
+var inputField2 = d3.select("#city");
+var tbody = d3.select("tbody");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// var jj = $('#datepicker1').datepicker('getDate');
+// console.log(jj);
+var populate = (dataInput) => {
+
+  dataInput.forEach(ufo_sightings => {
+    var row = tbody.append("tr");
+    columns.forEach(column => row.append("td").text(ufo_sightings[column])
+    )
+  });
+};
+/* $(document).ready(function() {
+	$('#datetimepicker1').datetimepicker();
+}); */
+
+//Populate  table
+populate(data);
+
+// Filter by attribute
+button.on("click", () => {
+  // console.log(inputField2);
+  d3.event.preventDefault();
+  // console.log(inputField1);
+  var inputDate = inputField1.property("value").trim();
+  // var inputDate = $("#datepicker1").datepicker('getDate');
+  // yy = $.datepicker.formatDate('dd/mm/yy', inputField1);
+  // console.log("Input date is "+ inputDate);
+  var inputCity = inputField2.property("value").toLowerCase().trim();
+  // Filter by field matching input value
+  // var filterDate = data.filter(data => data.datetime === inputDate);
+  var filterDate = data.filter( data =>  Date.parse(data.datetime) === Date.parse(inputDate));
+  // console.log(filterDate);
+  var filterCity = data.filter(data => data.city === inputCity );
+  // console.log(inputField2);
+  var filterData = data.filter(data => ( Date.parse(data.datetime) === Date.parse(inputDate) ) && ( data.city === inputCity ) );
+  // console.log(filterData);
+  // console.log("input city is "+ inputCity);
+
+  // Add filtered sighting to table
+  tbody.html("");
+
+  let response = {
+    filterData, filterCity, filterDate
+  };
+
+  if (response.filterData.length !== 0) {
+    populate(filterData);
+  }
+    else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+      populate(filterCity) || populate(filterDate);
+    }
+    else {
+      tbody.append("tr").append("td").text("No results found!"); 
+    }
+});
+
+resetbtn.on("click", () => {
+  tbody.html("");
+  populate(data);
+  console.log("Table reset");
+  console.log(inputField1);
+  var inputField1 = $("#datepicker1").datepicker('getDate');
+  $.datepicker.formatDate('dd-mm-yy', inputField1);
+
+  // console.log(inputField1);
+  // console.log(inputField2);
+});
